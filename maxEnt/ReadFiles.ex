3405274e46ea85a49ebd1991729bf2ab1ef6730d
ChrisHavens <havenscs@rose-hulman.edu>
2016-02-21 19:46:52 -0500

    Input works

--- maxEnt/ReadFiles.ex
+++ maxEnt/ReadFiles.ex
@@ -1,16 +1,7 @@
 defmodule ReadFiles do
     def getClass1Names do
         {_,files} = File.ls("./Class1")
-        addDoc = (fn(elem) -> Path.join("./Class1",elem) end)
-        fullName = (fn(elem) -> Path.absname(elem) end)
-        files
-        |>Enum.map(addDoc)
-        |>Enum.map(fullName)
-    end
-    
-    def getClass2Names do
-        {_,files} = File.ls("./Class2")
-        addDoc = (fn({first, second}) -> {Path.join("./Class2",fisrt),second} end)
+        addDoc = (fn({first, second}) -> {Path.join("./Class1",first),second} end)
         fullName = (fn({first, second}) -> {Path.absname(first),second} end)
         nameCopy = (fn(elem) -> {elem, elem} end)
         files
@@ -19,11 +10,15 @@ defmodule ReadFiles do
         |>Enum.map(fullName)
     end
     
-    def getFileName value do
-        {_,files} = File.ls(".")
-        nameCheck = (fn(elem) -> ((value == elem) || (value == Path.basename(elem))) end)
+    def getClass2Names do
+        {_,files} = File.ls("./Class2")
+        addDoc = (fn({first, second}) -> {Path.join("./Class2",first),second} end)
+        fullName = (fn({first, second}) -> {Path.absname(first),second} end)
+        nameCopy = (fn(elem) -> {elem, elem} end)
         files
-        |>Enum.filter(nameCheck)
+        |>Enum.map(nameCopy)
+        |>Enum.map(addDoc)
+        |>Enum.map(fullName)
     end
     
     def getTextFromName({fullPath, name}) do
@@ -31,12 +26,6 @@ defmodule ReadFiles do
         {name, text}
     end
     
-   # def displayFiles do
-   #     fileNames = getFileNames
-   #     files = Enum.map(fileNames, (fn(elem) -> 
-   #         {Path.basename(elem), File.read!(elem)} end))
-   #     IO.inspect(files)
-   # end
     
     def repl files do
         input = IO.gets("Enter search term or 'q' to quit: ")
@@ -44,21 +33,6 @@ defmodule ReadFiles do
             IO.inspect(input)
             repl files
         end
-    end
-    
-    def doWorkOnFiles(files, input) do
-        fun = (fn(elem) -> work(elem,input)end)
-        Enum.filter(files, fun)
-    end
-    
-    def work({filePath,_}, input) do
-        ext = Path.extname(filePath)
-        name = Path.basename(filePath,ext)
-        fileFirst = List.first(String.split(name))
-        inputFirst = List.first(String.split(input))
-        IO.inspect(fileFirst)
-        IO.inspect(inputFirst)
-        fileFirst == inputFirst
     end
     
     def main do
@@ -72,7 +46,7 @@ defmodule ReadFiles do
         IO.inspect(class1)
         IO.inspect(class2)
         
-        repl filesWithData
+        repl {class1,class2}
     end
         
     
